4988215f1325b19967bbff7ec9b1750553ff5fc3
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2021-02-05 12:06:40 -0800

    added the swagger for api documentation

--- src/main/java/com/javadaily/redditclone/RedditCloneApplication.java
+++ src/main/java/com/javadaily/redditclone/RedditCloneApplication.java
@@ -1,11 +1,15 @@
 package com.javadaily.redditclone;
 
+import com.javadaily.redditclone.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
--- target/generated-sources/annotations/com/javadaily/redditclone/mapper/SubredditMapperImpl.java
+++ target/generated-sources/annotations/com/javadaily/redditclone/mapper/SubredditMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-03T22:15:16-0800",
+    date = "2021-02-05T11:57:13-0800",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.3 (JetBrains s.r.o)"
 )
 @Component
